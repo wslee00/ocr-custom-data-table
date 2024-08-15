d1d847a0956cfbc4b03e74a8450cdeef12331005
William Lee <will.maps@gmail.com>
2024-08-14 22:00:11 -0400

    initialize opp contact roles edit page

---
+++ force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
@@ -0,0 +1,24 @@
+import { LightningElement } from "lwc";
+
+export default class OppContactRolesEdit extends LightningElement {
+    contactRoles = [
+        {
+            Id: "001000000000000",
+            Name: "Malachi Corley",
+            Role: "Business User",
+            IsPrimary: false,
+        },
+        {
+            Id: "001000000000001",
+            Name: "Quinnen Williams",
+            Role: "Decision Maker",
+            IsPrimary: true,
+        },
+        {
+            Id: "001000000000002",
+            Name: "Aaron Rodgers",
+            Role: "Sponsor",
+            IsPrimary: false,
+        },
+    ];
+}
