198e8fb1fd51ac41a39e95a6adabc1637f527964
William Lee <will.maps@gmail.com>
2024-08-14 22:24:51 -0400

    add record picker for contact selection

--- force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
+++ force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
@@ -4,18 +4,21 @@ export default class OppContactRolesEdit extends LightningElement {
     contactRoles = [
         {
             Id: '001000000000000',
+            ContactId: '003O900000WaRgnIAF',
             Name: 'Malachi Corley',
             Role: 'Business User',
             IsPrimary: false,
         },
         {
             Id: '001000000000001',
+            ContactId: '003O900000WaRldIAF',
             Name: 'Quinnen Williams',
             Role: 'Decision Maker',
             IsPrimary: true,
         },
         {
             Id: '001000000000002',
+            ContactId: '003O900000WaRnFIAV',
             Name: 'Aaron Rodgers',
             Role: 'Sponsor',
             IsPrimary: false,
