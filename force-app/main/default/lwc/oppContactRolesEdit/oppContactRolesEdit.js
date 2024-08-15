b13a6e9361a5468fda5c77cc64fcc18ba23cb6b2
William Lee <will.maps@gmail.com>
2024-08-14 22:19:44 -0400

    Add dropdown for role picklist

--- force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
+++ force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
@@ -1,24 +1,34 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
 export default class OppContactRolesEdit extends LightningElement {
     contactRoles = [
         {
-            Id: "001000000000000",
-            Name: "Malachi Corley",
-            Role: "Business User",
+            Id: '001000000000000',
+            Name: 'Malachi Corley',
+            Role: 'Business User',
             IsPrimary: false,
         },
         {
-            Id: "001000000000001",
-            Name: "Quinnen Williams",
-            Role: "Decision Maker",
+            Id: '001000000000001',
+            Name: 'Quinnen Williams',
+            Role: 'Decision Maker',
             IsPrimary: true,
         },
         {
-            Id: "001000000000002",
-            Name: "Aaron Rodgers",
-            Role: "Sponsor",
+            Id: '001000000000002',
+            Name: 'Aaron Rodgers',
+            Role: 'Sponsor',
             IsPrimary: false,
         },
     ];
+
+    roles = [
+        { label: 'Business User', value: 'Business User' },
+        { label: 'Decision Maker', value: 'Decision Maker' },
+        { label: 'Sponsor', value: 'Sponsor' },
+    ];
+
+    handleRoleChange(event) {
+        console.log(event.detail.value);
+    }
 }
