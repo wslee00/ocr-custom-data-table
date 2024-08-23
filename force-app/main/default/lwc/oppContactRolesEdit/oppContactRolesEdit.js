c7875082fd92c0b39e3b75eddc17fd35a9cd1413
William Lee <will.maps@gmail.com>
2024-08-22 23:00:04 -0400

    Get role options from database

--- force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
+++ force-app/main/default/lwc/oppContactRolesEdit/oppContactRolesEdit.js
@@ -1,10 +1,14 @@
-import getOppContactRoles from '@salesforce/apex/OppContactRolesEditController.getOppContactRoles';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { api, LightningElement, wire } from 'lwc';
+import { MASTER_RECORD_TYPE_ID } from 'c/recordTypeConstants';
+import getOppContactRoles from '@salesforce/apex/OppContactRolesEditController.getOppContactRoles';
+import ROLE_FIELD from '@salesforce/schema/OpportunityContactRole.Role';
 
 export default class OppContactRolesEdit extends LightningElement {
     @api recordId;
 
     contactRoles = [];
+    roleOptions = [];
 
     @wire(getOppContactRoles, { opportunityId: '$recordId' })
     processGetContactRoles({ error, data }) {
@@ -16,11 +20,20 @@ export default class OppContactRolesEdit extends LightningElement {
         }
     }
 
-    roles = [
-        { label: 'Business User', value: 'Business User' },
-        { label: 'Decision Maker', value: 'Decision Maker' },
-        { label: 'Sponsor', value: 'Sponsor' },
-    ];
+    @wire(getPicklistValues, {
+        recordTypeId: MASTER_RECORD_TYPE_ID,
+        fieldApiName: ROLE_FIELD,
+    })
+    processGetPicklistValues({ error, data }) {
+        if (data) {
+            this.roleOptions = data.values.map((rolePicklistValue) => {
+                return { label: rolePicklistValue.label, value: rolePicklistValue.value };
+            });
+        }
+        if (error) {
+            console.error(error);
+        }
+    }
 
     handleRoleChange(event) {
         console.log(event.detail.value);
